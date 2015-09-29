a435fdac377cb24a2e8084c4b8ce86f409ccb484
yfuka86 <yfuka86@gmail.com>
2015-09-29 21:29:49 +0900

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,3 +56,34 @@ task :update do
     template "unicorn.rb.erb", "#{shared_path}/config/unicorn.rb"
   end
 end
+
+# https://www.digitalocean.com/community/tutorials/how-to-install-cassandra-and-run-a-single-node-cluster-on-a-ubuntu-vps
+namespace :cassandra do
+  desc "install java and Cassandra"
+  task :install do
+    on roles(:app) do
+    end
+  end
+
+  %w[start stop restart].each do |command|
+    desc "#{command} Cassandra"
+    task command do
+      on roles(:app) do
+        execute :sudo, "/home/deploy/cassandra/tools/bin/cassandra-stressd #{command}"
+      end
+    end
+  end
+
+  desc "Setup Cassandra"
+  task :create do
+    on roles(:app) do
+      execute "RAILS_ENV=production bundle exec rake cequel:keyspace:create"
+    end
+  end
+
+  task :setup do
+    on roles(:app) do
+      execute "RAILS_ENV=production bundle exec rake cequel:migrate"
+    end
+  end
+end
